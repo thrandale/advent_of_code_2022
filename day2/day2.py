f2add677b8d545f3c9bd970b86d14c75b83af707
Theo Crandall <thrandale@gmail.com>
2022-12-02 23:50:54 -0700

    added day2 one-liners

--- day2/day2.py
+++ day2/day2.py
@@ -1,3 +1,5 @@
+import re
+
 input = [
     tuple(x for x in line.split(" ")) for line in open("input.txt").read().splitlines()
 ]
@@ -32,5 +34,9 @@ def getScore2(theirs, score):
         mine = theirs
     return mine + scores[score - 1]
 
+print("Part 1:", sum([getScore(types[theirs], types[mine]) for theirs, mine in input]))
+print("Part 2:", sum([getScore2(types[their], types[mine]) for their, mine in input]))
 
-print(sum([getScore2(types[their], types[mine]) for their, mine in input]))
+# solutions, but in single lines
+# print("Part 1:", sum([b + (3 if a == b else (6 if b == (a + 1) or b == (a - 2) else 0)) for a, b in [[int(x), int(y)] for x, y in [re.sub("A|X", "1", re.sub("B|Y", "2", re.sub("C|Z", "3", line))).split(" ") for line in open("input.txt").read().splitlines()]]]))
+# print("Part 2:", sum([3 * (b - 1) + ((a - 1 if a > 1 else 3) if b == 1 else ((a + 1 if a < 3 else 1) if b == 3 else a)) for a, b in [[int(x), int(y)] for x, y in [re.sub("A|X", "1", re.sub("B|Y", "2", re.sub("C|Z", "3", line))).split(" ") for line in open("input.txt").read().splitlines()]]]))
