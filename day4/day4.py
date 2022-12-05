02d7af8e6c85b4fa3bc5e87f7c1131b40f15d40d
Theo Crandall <thrandale@gmail.com>
2022-12-05 00:28:29 -0700

    some efficiency improvements

--- day4/day4.py
+++ day4/day4.py
@@ -1,13 +1,10 @@
-input = open("input.txt").read().splitlines()
+with open("input.txt") as file:
+    part1 = 0
+    part2 = 0
+    for line in file:
+        ranges = [list(map(int, range.split("-"))) for range in line.split(",")]
+        part1 += (ranges[0][0] - ranges[1][0]) * (ranges[0][1] - ranges[1][1]) <= 0
+        part2 += (ranges[0][0] <= ranges[1][1]) and (ranges[0][1] >= ranges[1][0])
 
-part1 = 0
-part2 = 0
-for line in input:
-    ranges = [
-        [int(x), int(y)] for x, y in [range.split("-") for range in line.split(",")]
-    ]
-    part1 += (ranges[0][0] - ranges[1][0]) * (ranges[0][1] - ranges[1][1]) <= 0
-    part2 += (ranges[0][0] <= ranges[1][1]) and (ranges[0][1] >= ranges[1][0])
-
-print("Part 1:", part1)
-print("Part 2:", part2)
+    print("Part 1:", part1)
+    print("Part 2:", part2)
