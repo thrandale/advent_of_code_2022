a6eb7cfc3f106c7e9dc42afde74de0bd825c9b31
Theo Crandall <thrandale@gmail.com>
2022-12-07 13:41:49 -0700

    fixed input

--- day1/day1.py
+++ day1/day1.py
@@ -3,7 +3,7 @@ print(
         sorted(
             [
                 sum([int(num) for num in elf.split()])
-                for elf in open("day1/input.txt").read().split("\n\n")
+                for elf in open("input.txt").read().split("\n\n")
             ],
             reverse=True,
         )[:3]
